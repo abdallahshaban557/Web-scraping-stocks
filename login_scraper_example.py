89cdf11735b2a0e9c67d434aacb4085d4fbcdb04
abdullahshaaban <abdullah.shaaban91@gmail.com>
2018-03-27 22:18:18 -0700

    Finished the script ! (If I only I didn't forget my charger...)

--- login_scraper_example.py
+++ login_scraper_example.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from openpyxl import Workbook
 from openpyxl import load_workbook
+import re
 
 #x = urllib.request.urlopen('https://pythonprogramming.net')
 
@@ -23,122 +24,91 @@ try:
         }
     })
     #Open the chrome driver, and navigate to the page
-    driver = webdriver.Chrome("chromedriver/chromedriver.exe",chrome_options=chrome_options)
-    driver.get("https://www.zacks.com/")
-
+    driver = webdriver.Chrome("newchromedriver/chromedriver.exe",chrome_options=chrome_options)
     
 
     #Load original Excel file - and select the needed sheet - get maximum row number
-    #Result_File = load_workbook('results_file/messedupb.xlsx')
-    Result_File = load_workbook('results_file/messedupb.xlsx')
+    Result_File = load_workbook('source_file/IPOs.xlsx')
     Result_sheet = Result_File.worksheets[0]
     Max_Row_Count = Result_sheet.max_row
 
-    #Count not found ticker EPS on site
+    #Count not found records  on site
     Not_Found_Tickers = 0
     
-    for i in range(2, Max_Row_Count):
+    #Filing type
+    Filing_Type = "424B4"
+
+    #Program Name
+    Program_Name = "directed share program"
+
+    for i in range(227, Max_Row_Count):
+
+        driver.get("https://www.sec.gov/edgar/searchedgar/companysearch.html")
+
         #Get the ticker and EPS date from data sheet
         print(i)  
 
         #Save a final results sheet
-        Result_File.save('results_file/FINAL.xlsx')  
+        if(i%10 == 0):
+            Result_File.save('results_file/FINAL.xlsx')  
 
-        Ticker_Symbol = Result_sheet.cell(row=i, column=6).value
+        Ticker_Symbol = Result_sheet.cell(row=i, column=3).value
         
-        #Get date from the initial file. Minus and add a day for better results
-        New_EPS_date_format = Result_sheet.cell(row=i, column=3).value.date()
-        EPS_Day = New_EPS_date_format.strftime('%#d')
-        EPS_Month = New_EPS_date_format.strftime('%#m')
-        EPS_Year = New_EPS_date_format.strftime('%Y')
-        EPS_Date = EPS_Month + "/" + EPS_Day + "/" + EPS_Year
-        
-        EPS_Date_plus_day = New_EPS_date_format + timedelta(days=1)
-        EPS_Day = EPS_Date_plus_day.strftime('%#d')
-        EPS_Month = EPS_Date_plus_day.strftime('%#m')
-        EPS_Year = EPS_Date_plus_day.strftime('%Y')
-        EPS_Date_plus_day = EPS_Month + "/" + EPS_Day + "/" + EPS_Year
 
-        EPS_Date_minus_day = New_EPS_date_format - timedelta(days=1)
-        EPS_Day = EPS_Date_minus_day.strftime('%#d')
-        EPS_Month = EPS_Date_minus_day.strftime('%#m')
-        EPS_Year = EPS_Date_minus_day.strftime('%Y')
-        EPS_Date_minus_day = EPS_Month + "/" + EPS_Day + "/" + EPS_Year
-
-        #Get all EPS values in data sheet
-        IBES_Estimize_actual = Result_sheet.cell(row=i, column=2).value
-        IBES_actual = Result_sheet.cell(row=i, column=4).value
-        IBES_adj_actual = Result_sheet.cell(row=i, column=5).value
 
         #Locate the search box on top of the page
-        Ticker_Symbol_Search = driver.find_element_by_name('search-q')
+        Ticker_Symbol_Search = driver.find_element_by_name("CIK")
 
         #send ticker symbol value to the search box
-        Ticker_Symbol_Search.send_keys(Ticker_Symbol)
+        Ticker_Symbol_Search.send_keys(str(Ticker_Symbol))
         
         #Press enter on the search box
         Ticker_Symbol_Search.send_keys(Keys.ENTER)
 
         try:
-            #find earnings announcement from the sidemenu
-            Earnings_Announcement = driver.find_element_by_xpath('//*[@id="left_rail"]/nav/div[2]/ul[3]/li[2]/ul/li[4]/a')
+            time.sleep(2)  
+            #Find the Filing type type search box
+            Filing_Type_Search = driver.find_element_by_name('type')
 
-            #Click on earnings announcement in the sidemenu
-            driver.execute_script("arguments[0].click();", Earnings_Announcement)
+            #send filing type to search box
+            Filing_Type_Search.send_keys(Filing_Type)
 
-            #Select the drop down row list and select 100 as the row number
-            Drop_Down_List = Select(driver.find_element_by_xpath('//*[@id="earnings_announcements_earnings_table_length"]/label/select'))
-            Drop_Down_List.select_by_visible_text('100')
+            #press enter on the search box
+            Filing_Type_Search.send_keys(Keys.ENTER)
 
-            time.sleep(1)
+            time.sleep(2)           
 
-            #Row number in the website EPS table
-            EPS_Table_Row_Number = 1
-        
-            #Go through the whole table of EPS
-            while True:
-                try:
-                    #Grab date from the website
-                    Date_Of_EPS = driver.find_element_by_xpath('//*[@id="earnings_announcements_earnings_table_wrapper"]/div[3]/div[3]/div[2]/div/table/tbody/tr['+ str(EPS_Table_Row_Number) +']/td').text
+            #Grab the documents link and click it
+            Documents_Link = driver.find_element_by_xpath('//*[@id="documentsbutton"]').click()
 
-                    #compare date from the data sheet with the website, and assign the right values based on EPS
-                    if Date_Of_EPS == EPS_Date or Date_Of_EPS == EPS_Date_minus_day or Date_Of_EPS == EPS_Date_plus_day:
-                        Reported_EPS = driver.find_element_by_xpath('//*[@id="earnings_announcements_earnings_table"]/tbody/tr[' + str(EPS_Table_Row_Number) + ']/td[4]').text
-                        if Reported_EPS != "--":
-                            Reported_EPS = Reported_EPS.replace("$","")
-                            Reported_EPS = float(Reported_EPS)
-                            Result_sheet.cell(row=i, column=11).value = Reported_EPS
-                        else:
-                            Result_sheet.cell(row=i, column=11).value = Reported_EPS
+            time.sleep(2)
+            #424B4 document link
+            Final_Document_Link = driver.find_element_by_xpath('//*[@id="formDiv"]/div/table/tbody/tr[2]/td[3]/a').click() 
 
-                        #Condition to add the value of IBES_RIGHT in data sheet
-                        if Reported_EPS == "--":
-                            Result_sheet.cell(row=i, column=9).value = 'N/A'
-                        elif IBES_actual == Reported_EPS or IBES_adj_actual == Reported_EPS:
-                            Result_sheet.cell(row=i, column=9).value = 1
-                        elif IBES_Estimize_actual != Reported_EPS:
-                            Result_sheet.cell(row=i, column=9).value = 0
-                        else:
-                            Result_sheet.cell(row=i, column=9).value = 0
-                         
-                        #Condition to add the value of ESTIMIZE to data sheet
-                        if Reported_EPS == "--":
-                            Result_sheet.cell(row=i, column=10).value = 'N/A'
-                        elif IBES_Estimize_actual == Reported_EPS:
-                            Result_sheet.cell(row=i, column=10).value = 1
-                        else:
-                            Result_sheet.cell(row=i, column=10).value = 0
 
-                    #Increment counter for loop
-                    EPS_Table_Row_Number += 1
-                   
+            time.sleep(2)
+            #get text from the page body
+            Body = driver.find_element_by_xpath("/html/body")
+            Body_To_Text = Body.text
+            Body_To_Text = Body_To_Text.lower()
+            
+
+            Program_Text = re.findall(r'([^.]*'+Program_Name+'[^.]*)', Body_To_Text)
+
+            if not Program_Text:
                 
-                except Exception as e:
-                    print(str(e))
-                    break         
+                Result_sheet.cell(row=i, column=12).value = 0
+                Result_sheet.cell(row=i, column=13).value = 'No Shares Program'
+                Result_sheet.cell(row=i, column=14).value = 'No Shares Program'
+
+            else:
+                Result_sheet.cell(row=i, column=12).value = 1
+                Result_sheet.cell(row=i, column=14).value = '.'.join(Program_Text)
+
         except Exception as e:
-            Result_sheet.cell(row=i, column=9).value = 'N/A'
-            Result_sheet.cell(row=i, column=10).value = 'N/A'
+            Result_sheet.cell(row=i, column=12).value = 'No SEC Record'
+            Result_sheet.cell(row=i, column=13).value = 'No SEC Record'
+            Result_sheet.cell(row=i, column=14).value = 'No SEC Record'
             Not_Found_Tickers += 1
             print('not found '+ str(Not_Found_Tickers))
             
