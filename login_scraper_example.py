381f4d89555cf6638bbb9d3bf4d56b030322daa2
abdullahshaaban <abdullah.shaaban91@gmail.com>
2018-04-26 22:45:46 -0700

    updated script

--- login_scraper_example.py
+++ login_scraper_example.py
@@ -23,6 +23,7 @@ try:
             'password_manager_enabled': False
         }
     })
+
     #Open the chrome driver, and navigate to the page
     driver = webdriver.Chrome("newchromedriver/chromedriver.exe",chrome_options=chrome_options)
     
@@ -41,7 +42,7 @@ try:
     #Program Name
     Program_Name = "directed share program"
 
-    for i in range(1650, Max_Row_Count):
+    for i in range(920, Max_Row_Count):
 
         driver.get("https://www.sec.gov/edgar/searchedgar/companysearch.html")
 
@@ -49,12 +50,10 @@ try:
         print(i)  
 
         #Save a final results sheet
-        if(i%10 == 0):
+        if(i%5 == 0):
             Result_File.save('results_file/FINAL.xlsx')  
 
         Ticker_Symbol = Result_sheet.cell(row=i, column=3).value
-        
-
 
         #Locate the search box on top of the page
         Ticker_Symbol_Search = driver.find_element_by_name("CIK")
@@ -78,13 +77,37 @@ try:
 
             time.sleep(2)           
 
-            #Grab the documents link and click it
-            Documents_Link = driver.find_element_by_xpath('//*[@id="documentsbutton"]').click()
+            #Finding number of rows in SEC Table
+            results_table = driver.find_element_by_class_name("tableFile2")
+            results_rows = len(results_table.find_elements_by_tag_name("tr"))
+            #IPO Date from the sheet
+            IPO_Date = Result_sheet.cell(row=i, column=1).value
+            #array of all results
+            date_calculation = []
+            #Find the required date for the IPO
+            for j in range (2, results_rows+1):
+                date_search_result = datetime.strptime(driver.find_element_by_xpath('//*[@id="seriesDiv"]/table/tbody/tr['+str(j)+']/td[4]').text, '%Y-%m-%d')
+                date_minus_search_result = IPO_Date - date_search_result
+                date_minus_search_result = date_minus_search_result.days
+                date_calculation.append(date_minus_search_result) 
+                
+            #Find the closest cell to the required
+            closest_date = min(date_calculation, key=abs)
+            #Get the index of the array that had the closest date
+            for z in range(0, len(date_calculation)):
+                if closest_date == date_calculation[z]:
+                    closest_date = z
+                    break
 
+            #Find the row that has the closest date
+            Document_cell = driver.find_element_by_xpath('//*[@id="seriesDiv"]/table/tbody/tr['+str(closest_date+2)+']/td[2]').click()
+            
             time.sleep(2)
-            #424B4 document link
-            Final_Document_Link = driver.find_element_by_xpath('//*[@id="formDiv"]/div/table/tbody/tr[2]/td[3]/a').click() 
 
+            #Grab the first link for the 424B4 and click it
+            Documents_Link = driver.find_element_by_xpath('//*[@id="formDiv"]/div/table/tbody/tr[2]/td[3]/a').click()
+
+    
 
             time.sleep(2)
             #get text from the page body
@@ -94,15 +117,23 @@ try:
             
 
             Program_Text = re.findall(r'([^.]*'+Program_Name+'[^.]*)', Body_To_Text)
+        
+            Program_Name = "directed shares"
+
+            Program_Text = Program_Text + re.findall(r'([^.]*'+Program_Name+'[^.]*)', Body_To_Text)
+
+            Program_Name = "directed unit program"
+
+            Program_Text = Program_Text + re.findall(r'([^.]*'+Program_Name+'[^.]*)', Body_To_Text)
 
             if not Program_Text:
-                
                 Result_sheet.cell(row=i, column=13).value = 0
                 Result_sheet.cell(row=i, column=14).value = 'No Shares Program'
                 Result_sheet.cell(row=i, column=15).value = 'No Shares Program'
 
             else:
                 Result_sheet.cell(row=i, column=13).value = 1
+                Result_sheet.cell(row=i, column=14).value = ''
                 Result_sheet.cell(row=i, column=15).value = '.'.join(Program_Text)
 
         except Exception as e:
